1c78b1aa9d7ea3c35c06ace2d80c41b6f63a559e
Graphity <rohood3@gmail.com>
2023-06-09 02:33:59 +0400

    Resize and load image using url from firestore

--- app/src/main/java/com/example/recyclerviewapplication1/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewapplication1/HomeRecyclerViewAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.recyclerviewapplication1
 
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewapplication1.databinding.ItemViewBinding
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
 
 class HomeRecyclerViewAdapter(val dataList : MutableList<Email>) : RecyclerView.Adapter<HomeRecyclerViewAdapter.HomeRecyclerViewViewHolder>() {
 
@@ -18,12 +21,30 @@ class HomeRecyclerViewAdapter(val dataList : MutableList<Email>) : RecyclerView.
     }
 
     override fun onBindViewHolder(holder: HomeRecyclerViewViewHolder, position: Int) {
+        val transformation: Transformation = object : Transformation {
+            override fun transform(source: Bitmap): Bitmap {
+                val desiredWidth = 100
+                val desiredHeight = 100
+
+                val scaledBitmap = Bitmap.createScaledBitmap(source, desiredWidth, desiredHeight, true)
+                if (scaledBitmap != source) {
+                    source.recycle()
+                }
+
+                return scaledBitmap
+            }
+
+            override fun key(): String {
+                return "resizeTransformation"
+            }
+        }
+
         val item = dataList[position]
         holder.binding.apply {
             emailAuthorTV.text = item.author
             emailSubjectTV.text = item.subject
             emailContentTV.text = item.content
-//            imageView.setImageResource(item.image!!)
+            Picasso.get().load(item.image).transform(transformation).into(imageView)
         }
     }
 
